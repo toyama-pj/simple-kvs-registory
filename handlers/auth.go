8dd7518caf651b7df78d35d83cfd9477e0949afb
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2026-07-06 04:35:30 +0900

    feat: add authentication handlers and one-time login logic

---
+++ handlers/auth.go
@@ -0,0 +1,200 @@
+package handlers
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/toyama-pj/simple-kvs-registory/lib"
+	"gorm.io/gorm"
+)
+
+type Controller struct {
+	DB     *gorm.DB
+	Config lib.Config
+}
+
+func NewController(db *gorm.DB, config lib.Config) *Controller {
+	return &Controller{DB: db, Config: config}
+}
+
+func (cont *Controller) ReturnLibController() *lib.Controller {
+	return lib.NewController(cont.DB, cont.Config)
+}
+
+// AuthHandlersSetup
+//
+// @Tag.name			auth
+// @Tag.description	認証・ログインに関するAPI群
+func (cont *Controller) AuthHandlersSetup(router fiber.Router) {
+	router.Post("/login", cont.PostLoginOneTimeCodeHandler)
+	router.Post("/login/callback", cont.PostLoginHandler)
+}
+
+type PostLoginOneTimeCodeRequestBody struct {
+	Email string `json:"email" validate:"required,email"`
+}
+
+// PostLoginOneTimeCodeHandler
+// @Summary		ログインワンタイムパスワードを生成
+// @Description	メールアドレスをキーとしてユーザを照合し、ログインワンタイムパスワードを生成・送信
+// @Accept		json
+// @Produce		json
+// @Param		request	body		PostLoginOneTimeCodeRequestBody	true	"Email"
+// @Success		204		{object}	nil				"成功（返却ボディなし）"
+// @Failure		400		{object}	lib.RFCErrorResponse
+// @Router		/auth/login [post]
+func (con *Controller) PostLoginOneTimeCodeHandler(c fiber.Ctx) error {
+	req := new(PostLoginOneTimeCodeRequestBody)
+	if err := c.Bind().All(req); err != nil {
+		return c.Status(400).JSON(
+			lib.NewRFCErrorResponse(
+				400,
+				"err/login/invalid_request",
+				"Request is not valid",
+				c.Path(),
+			),
+		)
+	}
+
+	if con.Config.DEVELOPMENT == true {
+		cont := con.ReturnLibController()
+		u, err := cont.GetUserByMailAddress(req.Email)
+		if err == gorm.ErrRecordNotFound {
+			return c.Status(fiber.StatusNoContent).JSON("{}")
+		}
+		if err != nil {
+			if con.Config.DEVELOPMENT == true {
+				return c.Status(fiber.StatusInternalServerError).JSON(
+					lib.NewRFCErrorResponse(
+						fiber.StatusInternalServerError,
+						"err/login/database_error_get_user",
+						err.Error(),
+						c.Path(),
+					),
+				)
+			}
+			return c.Status(fiber.StatusInternalServerError).JSON(
+				lib.NewRFCErrorResponse(
+					fiber.StatusInternalServerError,
+					"err/login/internal_error",
+					"Internal Server Error has occurred. Please retry later.",
+					c.Path(),
+				),
+			)
+		}
+		code, err := cont.CreateUserOneTimeLoginCode(u.ID)
+		if err != nil {
+			if con.Config.DEVELOPMENT == true {
+				return c.Status(fiber.StatusInternalServerError).JSON(
+					lib.NewRFCErrorResponse(
+						fiber.StatusInternalServerError,
+						"err/login/database_error_create_token",
+						err.Error(),
+						c.Path(),
+					),
+				)
+			}
+			return c.Status(fiber.StatusInternalServerError).JSON(
+				lib.NewRFCErrorResponse(
+					fiber.StatusInternalServerError,
+					"err/login/internal_error",
+					"Internal Server Error has occurred. Please retry later.",
+					c.Path(),
+				),
+			)
+		}
+		fmt.Println("Because of Development mode, One Time Login Code is not sending.")
+		fmt.Printf("Sent to %s, and Code is %s\n", req.Email, code)
+	} else {
+		return c.Status(fiber.StatusNotImplemented).JSON(
+			lib.NewRFCNotImplementErrorResponse(c.Path()),
+		)
+	}
+	return c.Status(fiber.StatusNoContent).JSON("{}")
+}
+
+type PostLoginRequestBody struct {
+	Email string `json:"email" validate:"required,email"`
+	Code  string `json:"code" validate:"required,len=6"`
+}
+
+// PostLoginHandler
+// @Summary	ログインワンタイムコードを検証
+// @Description	メールアドレスとログインワンタイムパスコードを検証し、Bearer Token を返却
+// @Accept		json
+// @Produce		json
+// @Param		request	body		PostLoginRequestBody	true	"Email and Code"
+// @Success		200		{object}	lib.UserBearerToken		"Bearer Token"
+// @Failure		400		{object}	lib.RFCErrorResponse
+// @Failure		401		{object}	lib.RFCErrorResponse
+// @Router		/auth/login/callback [post]
+func (con *Controller) PostLoginHandler(c fiber.Ctx) error {
+	req := new(PostLoginRequestBody)
+	if err := c.Bind().All(req); err != nil {
+		return c.Status(400).JSON(
+			lib.NewRFCErrorResponse(
+				400,
+				"err/login/invalid_request",
+				"Request is not valid",
+				c.Path(),
+			),
+		)
+	}
+
+	cont := con.ReturnLibController()
+	u, err := cont.GetUserOneTimeLoginCode(req.Email, req.Code)
+	if errors.Is(err, lib.ErrInvalidToken) {
+		return c.Status(fiber.StatusUnauthorized).JSON(
+			lib.NewRFCErrorResponse(
+				fiber.StatusUnauthorized,
+				"err/login/invalid_token",
+				"Invalid token",
+				c.Path(),
+			),
+		)
+	} else if err != nil {
+		if con.Config.DEVELOPMENT == true {
+			return c.Status(fiber.StatusInternalServerError).JSON(
+				lib.NewRFCErrorResponse(
+					fiber.StatusInternalServerError,
+					"err/login/database_error_get_user",
+					err.Error(),
+					c.Path(),
+				),
+			)
+		}
+		return c.Status(fiber.StatusInternalServerError).JSON(
+			lib.NewRFCErrorResponse(
+				fiber.StatusInternalServerError,
+				"err/login/internal_error",
+				"Internal Server Error has occurred. Please retry later.",
+				c.Path(),
+			),
+		)
+	}
+
+	token, err := cont.CreateUserBearerToken(u.ID)
+	if err != nil {
+		if con.Config.DEVELOPMENT == true {
+			return c.Status(fiber.StatusInternalServerError).JSON(
+				lib.NewRFCErrorResponse(
+					fiber.StatusInternalServerError,
+					"err/login/database_error_create_token",
+					err.Error(),
+					c.Path(),
+				),
+			)
+		}
+		return c.Status(fiber.StatusInternalServerError).JSON(
+			lib.NewRFCErrorResponse(
+				fiber.StatusInternalServerError,
+				"err/login/internal_error",
+				"Internal Server Error has occurred. Please retry later.",
+				c.Path(),
+			),
+		)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(token)
+}
---
+++ lib/auth.go
@@ -0,0 +1,284 @@
+package lib
+
+import (
+	"crypto/rand"
+	"errors"
+	"math/big"
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+var (
+	ErrInvalidToken = errors.New("invalid token")
+	ErrUserNotFound = errors.New("user not found")
+)
+
+type User struct {
+	ID        uuid.UUID `gorm:"primaryKey;type:uuid;column:id"`
+	Name      string    `gorm:"type:varchar;column:name"`
+	Email     string    `gorm:"type:varchar;column:email;uniqueIndex"`
+	CreatedAt time.Time `gorm:"type:timetz;column:created_at"`
+	UpdatedAt time.Time `gorm:"type:timetz;column:updated_at"`
+	DeletedAt gorm.DeletedAt
+}
+
+type UserOneTimeLogin struct {
+	ID        int       `gorm:"primaryKey;column:id;autoIncrement"`
+	UserID    uuid.UUID `gorm:"column:user_id"`
+	Token     string    `gorm:"type:varchar;column:token"`
+	CreatedAt time.Time `gorm:"type:timetz;column:created_at"`
+	ExpiresAt time.Time `gorm:"type:timetz;column:expires_at"`
+}
+
+type UserBearerToken struct {
+	ID        int       `gorm:"primaryKey;column:id;autoIncrement"`
+	UserID    uuid.UUID `gorm:"column:user_id"`
+	Token     string    `gorm:"type:varchar;column:token"`
+	CreatedAt time.Time `gorm:"type:timetz;column:created_at"`
+	UpdatedAt time.Time `gorm:"type:timetz;column:updated_at"`
+	ExpiresAt time.Time `gorm:"type:timetz;column:expires_at"`
+	DeletedAt gorm.DeletedAt
+}
+
+type NamespaceAccessPermission struct {
+	NamespaceID uuid.UUID `gorm:"primaryKey;type:uuid;column:namespace_id"`
+	UserID      uuid.UUID `gorm:"primaryKey;type:uuid;column:user_id"`
+	GrantType   string    `gorm:"column:grant_type"` // r, w, rw, admin
+	CreatedAt   time.Time `gorm:"type:timetz;column:created_at"`
+	DeletedAt   gorm.DeletedAt
+}
+
+func (UserOneTimeLogin) TableName() string {
+	return "user_one_time_login"
+}
+
+func (UserBearerToken) TableName() string {
+	return "user_bearer_token"
+}
+
+func (User) TableName() string {
+	return "user"
+}
+
+func createRandomDigit(n int) (string, error) {
+	bytes := make([]byte, n)
+	for i := 0; i < n; i++ {
+		val, err := rand.Int(rand.Reader, big.NewInt(10))
+		if err != nil {
+			return "", err
+		}
+		bytes[i] = '0' + byte(val.Int64())
+	}
+	return string(bytes), nil
+}
+
+func createRandomToken(char string, n int) (string, error) {
+	bytes := make([]byte, n)
+	for i := 0; i < n; i++ {
+		val, err := rand.Int(rand.Reader, big.NewInt(int64(len(char))))
+		if err != nil {
+			return "", err
+		}
+		bytes[i] = char[int(val.Int64())]
+	}
+	return string(bytes), nil
+}
+
+func (c Controller) CreateUser(name string, email string) error {
+	var userCount int64
+	err := c.DB.Model(&User{}).Where("email = ?", email).Count(&userCount).Error
+	if err != nil {
+		return err
+	}
+	if userCount > 0 {
+		return errors.New("user already exists")
+	}
+
+	user := User{
+		ID:    uuid.New(),
+		Name:  name,
+		Email: email,
+	}
+
+	return c.DB.Create(&user).Error
+}
+
+func (c Controller) CreateUserOneTimeLoginCode(userID uuid.UUID) (string, error) {
+	var count int64
+	err := c.DB.Model(&User{}).Where("id = ?", userID).Count(&count).Error
+	if err != nil {
+		return "", err
+	}
+	if count > 0 {
+		res, err := createRandomDigit(6)
+		if err != nil {
+			return "", err
+		}
+		u := UserOneTimeLogin{
+			UserID:    userID,
+			Token:     res,
+			CreatedAt: time.Now(),
+			ExpiresAt: time.Now().Add(time.Minute * 10),
+		}
+		err = c.DB.Create(&u).Error
+		if err != nil {
+			return "", err
+		}
+		return res, nil
+	}
+	return "", ErrUserNotFound
+}
+
+func (c Controller) GetUserOneTimeLoginCode(email string, token string) (User, error) {
+	var user User
+	err := c.DB.Where("email = ?", email).First(&user).Error
+	if err != nil {
+		return User{}, err
+	}
+
+	var onetime UserOneTimeLogin
+	err = c.DB.Where("token = ?", token).Where("expires_at > ?", time.Now()).Where("user_id = ?", user.ID).First(&onetime).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return User{}, ErrInvalidToken
+	}
+	if err != nil {
+		return User{}, err
+	}
+
+	err = c.DB.Model(&onetime).Update("expires_at", time.Now()).Error
+	if err != nil {
+		return User{}, err
+	}
+
+	return user, nil
+}
+
+func (c Controller) CreateUserBearerToken(userID uuid.UUID) (UserBearerToken, error) {
+	var count int64
+	err := c.DB.Model(&User{}).Where("id = ?", userID).Count(&count).Error
+	if err != nil {
+		return UserBearerToken{}, err
+	}
+	if count > 0 {
+		token, err := createRandomToken("abcdef0123456789", 32)
+		if err != nil {
+			return UserBearerToken{}, err
+		}
+		u := UserBearerToken{
+			UserID:    userID,
+			Token:     token,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			ExpiresAt: time.Now().Add(time.Hour * 24),
+		}
+		err = c.DB.Create(&u).Error
+		if err != nil {
+			return UserBearerToken{}, err
+		}
+		return u, nil
+	}
+	return UserBearerToken{}, ErrUserNotFound
+}
+
+func (c Controller) GetUserByMailAddress(email string) (User, error) {
+	var user User
+	err := c.DB.Where("email = ?", email).Where("deleted_at IS NULL").First(&user).Error
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
+
+func (c Controller) GetUserByUserBearerToken(token string) (User, error) {
+	var bearer UserBearerToken
+	err := c.DB.Where("token = ?", token).Where("expires_at > ?", time.Now()).Where("deleted_at IS NULL").First(&bearer).Error
+	if err != nil {
+		return User{}, err
+	}
+	var user User
+	err = c.DB.Where("id = ?", bearer.UserID).First(&user).Error
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
+
+func (c Controller) RevokeUserBearerToken(userId string) error {
+	err := c.DB.Model(&UserBearerToken{}).Where("user_id = ?", userId).Where("expires_at > ?", time.Now()).Update("deleted_at", time.Now()).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c Controller) PermitUserToAccessNamespace(doAsUserId string, targetUserId string, namespaceId string, grantType string) error {
+	if grantType != "r" && grantType != "w" && grantType != "rw" && grantType != "admin" && grantType != "none" {
+		return errors.New("invalid grant type")
+	}
+	err := c.DB.Model(&User{}).Where("id = ?", doAsUserId).First(&User{}).Error
+	if err != nil {
+		return err
+	}
+	var doAs NamespaceAccessPermission
+	err = c.DB.Model(&NamespaceAccessPermission{}).Where("namespace_id = ?", namespaceId).First(&doAs).Error
+	if err != nil {
+		return err
+	}
+	if doAs.GrantType != "admin" {
+		return errors.New("doAsUser is not administrator")
+	}
+	err = c.DB.Model(&User{}).Where("id = ?", targetUserId).First(&User{}).Error
+	if err != nil {
+		return err
+	}
+	var target NamespaceAccessPermission
+	tx := c.DB.Model(&NamespaceAccessPermission{}).Where("namespace_id = ?", namespaceId).First(&target)
+	err = tx.Error
+
+	if err == gorm.ErrRecordNotFound { // 新規作成
+		target = NamespaceAccessPermission{
+			NamespaceID: uuid.MustParse(namespaceId),
+			UserID:      uuid.MustParse(targetUserId),
+			GrantType:   grantType,
+			CreatedAt:   time.Now(),
+		}
+		err = c.DB.Create(target).Error
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err != nil {
+		return err
+	}
+
+	err = tx.Update("grant_type", grantType).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c Controller) CheckUserPermissionToAccessNamespace(userId string, namespaceId string) (bool, bool, bool, error) {
+	// read, write, admin (manage) の順に bool を返す
+	var target NamespaceAccessPermission
+	err := c.DB.Model(&NamespaceAccessPermission{}).Where("namespace_id = ?", namespaceId).Where("user_id = ?", userId).Where("deleted_at IS NULL").First(&target).Error
+	if err != nil {
+		return false, false, false, err
+	}
+	switch target.GrantType {
+	case "r":
+		return true, false, false, nil
+	case "w":
+		return true, true, false, nil
+	case "rw":
+		return true, true, false, nil
+	case "admin":
+		return true, true, true, nil
+	default:
+		return false, false, false, errors.New("invalid grant type")
+	}
+}
